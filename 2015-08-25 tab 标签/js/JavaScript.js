c970ded814df67ca02e1a15d91e137f2f11a1f4c
sunshine9 <1104506375@qq.com>
2015-08-25 11:03:54 +0800

    jquery tab 标签

---
+++ 2015-08-25 tab 标签/js/JavaScript.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    var $li = $(".container .mh li");
+    var $tab_main = $(".container .mc .main");
+
+    $li.mouseover(function () {
+        var $this = $(this);
+        var $index = $this.index();
+        $li.removeClass();
+        $this.addClass("tab_selected");
+
+        $tab_main.css("display", "none");
+        $tab_main.eq($index).css("display", "block");
+    })
+})
